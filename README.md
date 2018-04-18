cb6d7b8e5acb4b01b2e2882373a4b00c010d5488
geraosio <geraosio@gmail.com>
2018-04-18 04:56:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LabReservacion
+Prototype of a Lab Reservation System for Computer Science Department at Tec de Monterrey
---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Hide navbar after click/tap on a link
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
